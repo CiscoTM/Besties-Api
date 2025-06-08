443af76519f9be35a3154bfeb8c39a20a0aaad7d
Dev50 <franciscotortillol@gmail.com>
2025-06-08 08:11:24 +0200

    Initial commit

---
+++ src/main/java/org/dev50/Besties/BestiesApiApplication.java
@@ -0,0 +1,13 @@
+package org.dev50.Besties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev50/Besties/BestiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev50.Besties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
